b1c526b49d6eb2e94b86d5f148ec7132197fd22e
Carinou <carinamalleville@gmail.com>
2021-11-23 16:58:50 +0100

    fonction trouver_chemin

--- ford.ml
+++ ford.ml
@@ -1,8 +1,24 @@
+open Graph
+open Tools
+
 let init graph = assert false
     (*initialiser les flots et D à O*)
 
-let trouver_chemin graph node1 node2 = assert false
-    
+let rec trouver_chemin graph node1 node2 file =
+    (*y a pas encore node1 dans file au début*)
+    (* parcours en profondeur*)
+    if node1=node2 then node1::file
+    else
+        let voisins = out_arcs graph node1 in
+        let rec parcours_voisins liste_voisins =
+            (*vision récursive de la liste de voisins*)
+            (*vérifier si on trouve un chemin par là ou non*)
+            match liste_voisins with
+                |[] -> []
+                |x::rest -> if (trouver_chemin graph x node2 (x::file))=[] then parcours_voisins rest
+                            else (trouver_chemin graph x node2 (x::file)) (*à modifier*)
+            (*appel à parcours_voisins*)
+            (*finir la fonction rec en syntaxe*)
 
 let calcul_variation_flot chemin = assert false
     (*calcule la variation de flot d'un chemin*)
